300af6c8156fb4c1ec261039b1ebc3297fe56ca7
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2023-02-18 18:12:29 +0100

    Add handling of constants

--- vm.c
+++ vm.c
@@ -1,5 +1,7 @@
-#include "vm.h"
+#include <stdio.h>
+
 #include "common.h"
+#include "vm.h"
 
 VM vm;
 
@@ -9,10 +11,17 @@ void freeVM() {}
 
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
   for (;;) {
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
+    case OP_CONSTANT: {
+      Value constant = READ_CONSTANT();
+      printValue(constant);
+      printf("\n");
+      break;
+    }
     case OP_RETURN: {
       return INTERPRET_OK;
     }
@@ -20,6 +29,7 @@ static InterpretResult run() {
   }
 
 #undef READ_BYTE
+#undef READ_CONSTANT
 }
 
 InterpretResult interpret(Chunk *chunk) {
