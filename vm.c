1b54e2636dfee98e935b4d6300b9b9b276c3195f
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2023-02-18 18:23:52 +0100

    Prepare debugging

--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "debug.h"
 #include "vm.h"
 
 VM vm;
@@ -12,6 +13,10 @@ void freeVM() {}
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+#ifdef DEBUG_TRACE_EXECUTION
+  disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
 
   for (;;) {
     uint8_t instruction;
