68af1a22b469b7549b5ea4f6f836c235285de6ef
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2023-02-22 21:23:50 +0100

    Arithmetic binary OP

--- vm.c
+++ vm.c
@@ -25,6 +25,13 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
+
 
 #ifdef DEBUG_TRACE_EXECUTION
   printf("          ");
@@ -45,7 +52,21 @@ static InterpretResult run() {
       push(constant);
       break;
     }
-    case OP_NEGATE:   push(-pop()); break;
+    case OP_ADD:
+      BINARY_OP(+);
+      break;
+    case OP_SUBTRACT:
+      BINARY_OP(-);
+      break;
+    case OP_MULTIPLY:
+      BINARY_OP(*);
+      break;
+    case OP_DIVIDE:
+      BINARY_OP(/);
+      break;
+    case OP_NEGATE:
+      push(-pop());
+      break;
     case OP_RETURN: {
       printValue(pop());
       printf("\n");
@@ -56,6 +77,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk) {
