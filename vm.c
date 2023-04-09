d83fea7fdd29b2cd183974b7968a521a88c868cb
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2023-04-09 18:52:04 +0200

    Negate unary expressions and introduce simple error handling

--- vm.c
+++ vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "common.h"
@@ -8,6 +9,20 @@
 VM vm;
 
 static void resetStack() { vm.stackTop = vm.stack; }
+
+static void runtimeError(const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  fputs("\n", stderr);
+
+  size_t instruction = vm.ip - vm.chunk->code - 1;
+  int line = vm.chunk->lines[instruction];
+  fprintf(stderr, "[line %d] in script\n", line);
+  resetStack();
+}
+
 
 void initVM() { resetStack(); }
 
@@ -21,6 +36,10 @@ void push(Value value) {
 Value pop() {
   vm.stackTop--;
   return *vm.stackTop;
+}
+
+static Value peek(int distance) {
+  return vm.stackTop[-1 - distance];
 }
 
 static InterpretResult run() {
@@ -65,9 +84,13 @@ static InterpretResult run() {
     case OP_DIVIDE:
       BINARY_OP(/);
       break;
-    case OP_NEGATE:
-      push(-pop());
-      break;
+     case OP_NEGATE:
+        if (!IS_NUMBER(peek(0))) {
+          runtimeError("Operand must be a number.");
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        push(NUMBER_VAL(-AS_NUMBER(pop())));
+        break;
     case OP_RETURN: {
       printValue(pop());
       printf("\n");
