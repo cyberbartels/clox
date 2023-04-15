c8d45dbd8da80aa18f8398046928622c4954cf6d
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2023-04-15 07:30:19 +0200

    String concatenation

--- object.h
+++ object.h
@@ -24,7 +24,8 @@ struct ObjString {
   char *chars;
 };
 
-ObjString* copyString(const char* chars, int length);
+ObjString *takeString(char *chars, int length);
+ObjString *copyString(const char *chars, int length);
 void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
