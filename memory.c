6e59780e43f2dc21ec2a49270416cb882d40f7a7
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2024-03-17 18:13:45 +0100

    Add method references

--- memory.c
+++ memory.c
@@ -76,6 +76,12 @@ static void blackenObject(Obj *object) {
 #endif
 
   switch (object->type) {
+  case OBJ_BOUND_METHOD: {
+    ObjBoundMethod *bound = (ObjBoundMethod *)object;
+    markValue(bound->receiver);
+    markObject((Obj *)bound->method);
+    break;
+  }
   case OBJ_CLASS: {
     ObjClass *klass = (ObjClass *)object;
     markObject((Obj *)klass->name);
@@ -117,6 +123,9 @@ static void freeObject(Obj *object) {
 #endif
 
   switch (object->type) {
+  case OBJ_BOUND_METHOD:
+    FREE(ObjBoundMethod, object);
+    break;
   case OBJ_CLASS: {
     ObjClass *klass = (ObjClass *)object;
     freeTable(&klass->methods);
